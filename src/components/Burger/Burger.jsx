bb256584eee4c4f5e2ba709f8e9407bd6a049c73
rishavk1102 <rishavk1102@gmail.com>
2020-06-12 12:43:56 +0530

    Build controls UI

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,11 +4,19 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((prev, curr) => {
+      return prev.concat(curr);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
